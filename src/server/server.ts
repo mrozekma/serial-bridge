f74011efe47ac88c4e154ba957142c9a1553df3a
Michael Mrozek <git@mrozekma.com>
2019-10-10 22:51:42 -0400

    Move all the web-related code into web.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,9 @@
-import { Application } from '@feathersjs/express';
-import { Request, Response, NextFunction } from 'express-serve-static-core';
 import ora from 'ora';
 import slugify from 'slugify';
-import Url from 'url-parse';
 
 import banner from 'raw-loader!./banner.txt';
 import { loadConfig, Config } from './config';
 import Device from './device';
-import { ServerServices as Services, ServiceDefinitions } from '@/services';
 
 // From DefinePlugin
 declare const BUILD_VERSION: string, BUILD_DATE: string;
@@ -34,28 +30,6 @@ function makeDevice(deviceConfig: Config['devices'][number], existingIds?: Set<s
 	return device;
 }
 
-function makeServices(app: Application<Services>, devices: Device[]): ServiceDefinitions {
-	return {
-		'api/devices': {
-			events: [ 'test' ],
-			async find(params) {
-				return devices;
-			},
-			async get(id, params) {
-				const device = devices.find(device => device.id === id);
-				if(device) {
-					return device;
-				}
-				throw new Error(`Device not found: ${id}`);
-			},
-			async update(id, data, params) {
-				//TODO
-				throw new Error('Unimplemented');
-			}
-		},
-	};
-}
-
 (async () => {
 	console.log(`${banner}\n${BUILD_VERSION}\nBuilt ${BUILD_DATE}\n`);
 	const config = await spinner("Load configuration", loadConfig);
@@ -65,50 +39,12 @@ function makeServices(app: Application<Services>, devices: Device[]): ServiceDef
 		return config.devices.map(deviceConfig => makeDevice(deviceConfig, ids));
 	});
 	if(config.webPort !== undefined) {
-		const devicesRoute = /^\/devices\/([^/]+)\/?$/;
 		const app = await spinner("Make webserver", async () => {
 			const { makeWebserver } = await import(/* webpackChunkName: 'web' */ './web');
-			return makeWebserver(app => {
-				// Register a rewriter for /devices/:id for all valid IDs
-				app.use((req: Request<any>, res: Response, next: NextFunction) => {
-					const match = req.url.match(devicesRoute);
-					if(match && devices.find(device => device.id === match[1])) {
-						req.url = '/';
-					}
-					next();
-				});
-
-				// Register services
-				const services = makeServices(app, devices);
-				for(const [ name, service ] of Object.entries(services)) {
-					app.use(name, service);
-				}
-			});
+			return makeWebserver(devices);
 		});
 		app.listen(config.webPort);
 		console.log(`Listening on ${config.webPort}`);
-		app.on('connection', connection => {
-			const { pathname } = new Url((connection as any).headers.referer);
-			const match = pathname.match(devicesRoute);
-			if(match && devices.find(device => device.id === match[1])) {
-				app.channel(`device/${match[1]}`).join(connection);
-			}
-		});
 
-		app.service('api/devices').publish(data => {
-			const id = data.device;
-			if(id === undefined) {
-				throw new Error('Device service event missing device ID');
-			}
-			return [
-				app.channel('everyone'),
-				app.channel(`device/${id}`),
-			];
-		});
-		let i = 0;
-		const device = devices.find(device => device.id == 'test-device')!;
-		setInterval(() => {
-			device.emit(app, 'test', { i: i++ });
-		}, 1000);
 	}
 })().catch(console.error);
--- src/server/web.ts
+++ src/server/web.ts
@@ -3,10 +3,36 @@ import express, { Application } from '@feathersjs/express';
 import socketio from '@feathersjs/socketio';
 import '@feathersjs/transport-commons'; // Adds channel typing to express.Application
 import { Request, Response, NextFunction } from 'express-serve-static-core';
+import Url from 'url-parse';
 
-import { ServerServices as Services } from '@/services';
+import { ServerServices as Services, ServiceDefinitions } from '@/services';
+import Device from './device';
 
-export function makeWebserver(attachFn?: (app: Application<Services>) => void): Application<Services> {
+const devicesRoute = /^\/devices\/([^/]+)\/?$/;
+
+function makeServices(app: Application<Services>, devices: Device[]): ServiceDefinitions {
+	return {
+		'api/devices': {
+			events: [ 'test' ],
+			async find(params) {
+				return devices;
+			},
+			async get(id, params) {
+				const device = devices.find(device => device.id === id);
+				if(device) {
+					return device;
+				}
+				throw new Error(`Device not found: ${id}`);
+			},
+			async update(id, data, params) {
+				//TODO
+				throw new Error('Unimplemented');
+			}
+		},
+	};
+}
+
+export function makeWebserver(devices: Device[]): Application<Services> {
 	const app = express(feathers<Services>());
 
 	//TODO Figure out how to only allow CORS for REST endpoints
@@ -17,12 +43,23 @@ export function makeWebserver(attachFn?: (app: Application<Services>) => void): 
 	app.configure(express.rest());
 
 	app.configure(socketio(io => {}));
-	app.on('connection', connection => app.channel('everybody').join(connection));
+	// app.on('connection', connection => app.channel('everybody').join(connection));
 	// app.publish((data, hook) => app.channel('everybody'));
 	app.publish(data => { throw new Error(`Unexpected root-app publish: ${data}`); });
 
-	if(attachFn) {
-		attachFn(app);
+	// Register a rewriter for /devices/:id for all valid IDs
+	app.use((req: Request<any>, res: Response, next: NextFunction) => {
+		const match = req.url.match(devicesRoute);
+		if(match && devices.find(device => device.id === match[1])) {
+			req.url = '/';
+		}
+		next();
+	});
+
+	// Register services
+	const services = makeServices(app, devices);
+	for(const [ name, service ] of Object.entries(services)) {
+		app.use(name, service);
 	}
 
 	app.use(express.static('dist/client'));
@@ -36,6 +73,23 @@ export function makeWebserver(attachFn?: (app: Application<Services>) => void): 
 			console.error(err);
 		}
 		errorHandler(err, req, res, next);
+	});
+
+	// If a connection comes in from /device/:id, join that device's channel
+	app.on('connection', connection => {
+		const { pathname } = new Url((connection as any).headers.referer);
+		const match = pathname.match(devicesRoute);
+		if(match && devices.find(device => device.id === match[1])) {
+			app.channel(`device/${match[1]}`).join(connection);
+		}
+	});
+
+	app.service('api/devices').publish(data => {
+		const id = data.device;
+		if(id === undefined) {
+			throw new Error('Device service event missing device ID');
+		}
+		return app.channel(`device/${id}`);
 	});
 
 	return app;
