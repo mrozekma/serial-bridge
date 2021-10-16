795e3686384fb0ab1c3c4ff62811070a614f14bc
Michael Mrozek <git@mrozekma.com>
2021-10-16 08:59:22 +0000

    Sort the navbar devices menu and try to keep it somewhat short. Fixes T91

--- src/client/components/navbar.vue
+++ src/client/components/navbar.vue
@@ -13,11 +13,20 @@
 			</template>
 			<a-menu-item v-if="devices.state == 'pending'" disabled><a-spin size="small"/> Loading...</a-menu-item>
 			<a-menu-item v-else-if="devices.state == 'rejected'"  disabled><i class="fas fa-exclamation-circle"></i> Failed to load</a-menu-item>
-			<a-menu-item v-else v-for="device in filteredDevices" :key="device.name">
-				<a :href="getDeviceUrl(device)">
-					{{ device.name }}
-				</a>
-			</a-menu-item>
+			<template v-else v-for="menu in organizedDevices">
+				<a-sub-menu v-if="menu.name" :key="menu.name" :title="menu.name">
+					<a-menu-item v-for="device in menu.devices" :key="device.name">
+						<a :href="getDeviceUrl(device)">
+							{{ device.name }}
+						</a>
+					</a-menu-item>
+				</a-sub-menu>
+				<a-menu-item v-else v-for="device in menu.devices" :key="device.name">
+					<a :href="getDeviceUrl(device)">
+						{{ device.name }}
+					</a>
+				</a-menu-item>
+			</template>
 		</a-sub-menu>
 		<slot/>
 		<a-menu-item class="right faux">
@@ -35,9 +44,14 @@
 	placements.bottomLeft.offset = [0, 2];
 	placements.rightTop.offset = [0, 0];
 
-	import { isErrorDevice } from '../device-functions';
+	import { compareStrings, isErrorDevice } from '../device-functions';
 	import { appName, rootDataComputeds } from '../root-data';
 	import { DeviceJson } from '@/services';
+
+	interface DeviceMenu {
+		name?: string;
+		devices: DeviceJson[];
+	}
 
 	import SbCommandMenu from '../components/command-menu.vue';
 	export default Vue.extend({
@@ -48,12 +62,56 @@
 				default: appName,
 			},
 			title: String,
-			paused: Boolean,
 		},
 		computed: {
 			...rootDataComputeds(),
-			filteredDevices(): DeviceJson[] {
-				return (this.devices.state === 'resolved') ? this.devices.value.filter(device => !isErrorDevice(device)) : [];
+			organizedDevices(): DeviceMenu[] {
+				if(this.devices.state !== 'resolved') {
+					return [];
+				}
+				const maxLength = 10; // Somewhat arbitrary
+				const devices = this.devices.value.filter(device => !isErrorDevice(device)).sort((a, b) => compareStrings(a.name, b.name));
+				const rtn: DeviceMenu[] = [];
+				// The goal here is to keep the root Devices menu somewhat short. It might still have long submenus, but oh well.
+				if(devices.length <= maxLength) {
+					// Just put all the devices in the root menu
+					rtn.push({ devices });
+				} else {
+					// Organize devices by category
+					for(const device of devices) {
+						const category = device.category ?? 'Other';
+						const menu = rtn.find(e => e.name == category);
+						if(menu) {
+							menu.devices.push(device);
+						} else {
+							rtn.push({
+								name: category,
+								devices: [ device ],
+							});
+						}
+					}
+					rtn.sort((a, b) =>
+						(a.name === b.name) ? 0 :
+						(a.name === 'Other') ? 1 :
+						(b.name === 'Other') ? -1 :
+						compareStrings(a.name, b.name));
+					if(rtn.length > maxLength) {
+						// Too many categories
+						const removed = rtn.splice(maxLength - 1, rtn.length - (maxLength - 1));
+						rtn.push({
+							name: '...',
+							devices: [ ...removed.flatMap(e => e.devices) ],
+						});
+					} else if(rtn.length == 1 && rtn[0].devices.length > maxLength) {
+						// No categories, and too many devices that will end up in the root menu
+						rtn[0].name = undefined;
+						rtn.push({
+							name: '...',
+							devices: rtn[0].devices.splice(maxLength - 1, rtn[0].devices.length - (maxLength - 1)),
+						});
+					}
+				}
+				return rtn;
 			},
 		},
 		watch: {
