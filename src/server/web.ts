mrozekma/serial-bridge

commit 813bdd48e27f1d15d5b354db42eb9cb20e1b08a3

Support recreating devices based on the latest configuration file contents. Fixes T82

src/server/web.ts
@@ -10,8 +10,8 @@ import slugify from 'slugify';
 
 import { ServerServices as Services, ServiceDefinitions, DeviceJson } from '@/services';
 import { isBlacklisted, getBlacklist } from './blacklist';
-import { Config } from './config';
-import Device, { Node, Remote, EphemeralDevice, RemoteIONode } from './device';
+import { Config, gitPull, hasGitDir, loadConfig } from './config';
+import Device, { Node, Remote, EphemeralDevice, RemoteIONode, Devices, DevicesConfigReloadSpec } from './device';
 import { getUser, setUserInfo } from './connections';
 import Command, { iterCommands } from './command';
 import makeSetupZip from './setup-zip';
@@ -25,8 +25,9 @@ declare const BUILD_VERSION: string, BUILD_LINK: string, BUILD_ID: string | unde
 
 const devicesRoute = /^\/devices\/([^/]+)(?:\/manage)?\/?$/;
 const portsRoute = /^\/ports(?:\/find)?\/?$/;
+const configRoute = /^\/config\/reload\/?$/;
 
-function makeServices(app: Application<Services>, config: Config, devices: Device[], remotes: Remote[], commands: Command[], saveStore: SavedTerminalStore): ServiceDefinitions {
+function makeServices(app: Application<Services>, config: Config, devices: Devices, remotes: Remote[], commands: Command[], saveStore: SavedTerminalStore): ServiceDefinitions {
 	function getJenkinsDevice(name: any) {
 		const device = devices.find(device => device.jenkinsLockName == name);
 		if(device) {
@@ -101,16 +102,12 @@ function makeServices(app: Application<Services>, config: Config, devices: Devic
 					device.addNode(node);
 					node.tcpPort.open();
 				}
-				attachDeviceListeners(app, device);
 				device.on('updated', () => {
 					if(!device.alive) {
-						const idx = devices.findIndex(d => d === device);
-						if(idx >= 0) {
-							devices.splice(idx, 1);
-						}
+						devices.remove(device);
 					}
 				});
-				devices.push(device);
+				devices.add(device);
 				// There are some ports that need to be open before we serialize the device's JSON, but there's no good way to wait on them.
 				// Instead we just wait a tick.
 				await new Promise(resolve => process.nextTick(resolve));
@@ -186,8 +183,42 @@ function makeServices(app: Application<Services>, config: Config, devices: Devic
 					};
 				case 'blacklist':
 					return getBlacklist();
+				case 'reload':
+					if(!config.configReloadable) {
+						return {
+							enabled: false,
+							git: false,
+							add: [],
+							change: {},
+						};
+					}
+					const newConfig = await loadConfig();
+					return {
+						...devices.reloadConfig(newConfig.devices),
+						enabled: true,
+						git: await hasGitDir(),
+					};
 				}
 				throw new Error(`Config not found: ${id}`);
+			},
+			async patch(id, data: DevicesConfigReloadSpec, params) {
+				switch(id) {
+				case 'reload':
+					if(!config.configReloadable) {
+						throw new Error("Tool disabled");
+					} else if(!data || !data.add || !data.change) {
+						throw new Error("Bad parameters");
+					}
+					const newConfig = await loadConfig();
+					devices.reloadConfig(newConfig.devices, data);
+					break;
+				case 'reload/pull':
+					if(!config.configReloadable) {
+						throw new Error("Tool disabled");
+					}
+					await gitPull();
+					break;
+				}
 			},
 		},
 
@@ -351,7 +382,7 @@ function makeServices(app: Application<Services>, config: Config, devices: Devic
 	return services;
 }
 
-function makeRawListeners(socket: SocketIO.Socket, devices: Device[], commands: Command[]) {
+function makeRawListeners(socket: SocketIO.Socket, devices: Devices, commands: Command[]) {
 	//TODO I think there are some other events shoehorned into services that should be moved here
 	socket.on('node-stdin', (deviceId: string, nodeName: string, data: string) => {
 		const device = devices.find(device => device.id == deviceId);
@@ -380,12 +411,9 @@ function makeRawListeners(socket: SocketIO.Socket, devices: Device[], commands: 
 	});
 }
 
-function attachDeviceListeners(app: Application<Services>, devices: Device | Device[]) {
+function attachDeviceListeners(app: Application<Services>, devices: Device | Devices) {
 	const devicesService = app.service('api/devices');
-	if(!Array.isArray(devices)) {
-		devices = [ devices ];
-	}
-	for(const device of devices) {
+	for(const device of (Device.isDevice(devices) ? [ devices ] : devices)) {
 		const sendUpdate = () => devicesService.emit('updated', {
 			id: device.id,
 			remote: false,
@@ -427,7 +455,7 @@ function attachRemoteListeners(app: Application<Services>, remotes: Remote[]) {
 	}
 }
 
-export function makeWebserver(config: Config, devices: Device[], remotes: Remote[], commands: Command[]): Application<Services> {
+export function makeWebserver(config: Config, devices: Devices, remotes: Remote[], commands: Command[]): Application<Services> {
 	const app = express(feathers<Services>());
 
 	app.use((req, res, next) => {
@@ -548,8 +576,8 @@ export function makeWebserver(config: Config, devices: Device[], remotes: Remote
 		if(match && devices.find(device => device.id === match[1])) {
 			req.url = '/';
 		}
-		// Also the ports routes
-		if(portsRoute.test(pathname)) {
+		// Also the static routes
+		if([ portsRoute, configRoute ].some(pat => pat.test(pathname))) {
 			req.url = '/';
 		}
 		next();
@@ -676,6 +704,7 @@ export function makeWebserver(config: Config, devices: Device[], remotes: Remote
 
 	attachDeviceListeners(app, devices);
 	attachRemoteListeners(app, remotes);
+	devices.on('added', (devices: Devices, device: Device) => attachDeviceListeners(app, device));
 	if(config.portsFind.enabled) {
 		const portsService = app.service('api/ports');
 		onPortData((port, data) => portsService.emit('data', { path: port.path, data }));

config.example
@@ -41,6 +41,9 @@ config = {
 		},
 	},
 
+	// Optional. Enable reloading the configuration file to update devices without restarting the server.
+	configReloadable: false,
+
 	// An array of devices to connect to.
 	devices: [
 		{

src/client/app.vue
@@ -42,6 +42,7 @@
 	import ManageView from './views/manage.vue';
 	import PortsView from './views/ports.vue';
 	import PortsFindView from './views/ports-find.vue';
+	import ConfigReloadView from './views/config-reload.vue';
 	export default Vue.extend({
 		components: { SbNavbar, SbCommandPalette },
 		computed: {
@@ -65,6 +66,8 @@
 				view = PortsView;
 			} else if(path == '/ports/find') {
 				view = PortsFindView;
+			} else if(path == '/config/reload') {
+				view = ConfigReloadView;
 			} else {
 				error = `No view for route: ${path}`;
 			}
@@ -110,6 +113,11 @@
 					value: 'find-ports',
 					text: 'Find ports',
 					handler: () => window.location.assign('/ports/find'),
+				};
+				yield {
+					value: 'reload-config',
+					text: 'Reload configuration',
+					handler: () => window.location.assign('/config/reload'),
 				};
 			});
 		},

src/client/components/connection.vue
@@ -1,0 +1,55 @@
+<template>
+	<a-tooltip placement="bottomRight">
+		<template slot="title">
+			<div class="name">{{ name }}</div>
+			<div class="host" v-if="host != name">{{ host }}</div>
+			<div class="nodes">
+				<a-tag v-for="node in nodes" :key="node">{{ node }}</a-tag>
+			</div>
+		</template>
+		<a-avatar shape="square" :size="32" icon="user" :src="avatar" />
+	</a-tooltip>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	export default Vue.extend({
+		// These fields match up with the Connection interface from src/client/connection.ts
+		props: {
+			host: {
+				type: String,
+				required: true,
+			},
+			name: {
+				type: String,
+				required: true,
+			},
+			nodes: {
+				type: Array,
+				default: [],
+			},
+			avatar: {
+				type: String,
+			},
+		},
+	});
+</script>
+
+<style lang="less" scoped>
+	.ant-avatar {
+		margin-right: 8px;
+	}
+
+	.name {
+		font-weight: bold;
+	}
+
+	.host {
+		margin-top: -4px;
+		font-size: smaller;
+	}
+
+	.ant-tag:not(:first-child) {
+		margin-left: 5px;
+	}
+</style>

src/client/components/navbar.vue
@@ -28,6 +28,11 @@
 				</a-menu-item>
 			</template>
 		</a-sub-menu>
+		<a-sub-menu v-if="globalManage" title="Manage">
+			<a-menu-item><a href="/ports">Full port list</a></a-menu-item>
+			<a-menu-item><a href="/ports/find">Find ports</a></a-menu-item>
+			<a-menu-item><a href="/config/reload">Reload configuration</a></a-menu-item>
+		</a-sub-menu>
 		<slot/>
 		<a-menu-item class="right faux">
 			<slot name="right"/>
@@ -62,6 +67,10 @@
 				default: appName,
 			},
 			title: String,
+			globalManage: {
+				type: Boolean,
+				default: true,
+			},
 		},
 		computed: {
 			...rootDataComputeds(),

src/client/views/config-reload.vue
@@ -1,0 +1,222 @@
+<template>
+	<div>
+		<sb-navbar/>
+		<main>
+			<h1>Reload configuration</h1>
+			<a-alert v-if="preview.state === 'rejected'" type="error" message="Failed to check configuration file for changes" show-icon>
+				<template #description>
+					<pre>{{ preview.error.message }}</pre>
+					<a-button type="primary" @click="loadConfig">Retry</a-button>
+				</template>
+			</a-alert>
+			<a-alert v-else-if="preview.state === 'resolved' && !preview.value.enabled" type="error" message="Tool disabled" show-icon>
+				<template #description>
+					The config reload tool is disabled in Serial Bridge's configuration. See the <samp>configReloadable</samp> key in the configuration file.
+				</template>
+			</a-alert>
+			<template v-else>
+				<p>
+					The following devices have been changed in the configuration file since the last load. Select the devices to (re)create from the new configuration file.
+				</p>
+				<a-alert type="warning" message="Any existing devices that are reloaded will briefly go offline, and TCP connections will break. Devices that have been removed from the configuration will become permanently unavailable." show-icon/>
+				<a-alert v-if="preview.state === 'resolved' && preview.value.git && pulled !== true" type="info" show-icon>
+					<template #message>
+						Serial Bridge appears to be running from a revision-controlled directory. You may want to <a-button size="small" :loading="pulled === 'pending'" @click="gitPull">git pull</a-button> to get the latest changes to the config file.
+					</template>
+				</a-alert>
+				<a-table :columns="columns" :data-source="rows" :row-key="row => row.key" :loading="preview.state === 'pending'" :pagination="false" :locale="{ emptyText: 'No changes' }" :row-selection="{ selectedRowKeys: selections, onChange: keys => selections = keys }">
+					<template #type="type">
+						<a-tag v-if="type === 'add'" color="green">Add</a-tag>
+						<a-tag v-else-if="type === 'update'" color="orange">Update</a-tag>
+						<a-tag v-else-if="type === 'remove'" color="red">Remove</a-tag>
+					</template>
+					<template #connections="connections">
+						<sb-connection v-for="connection in (connections || [])" :key="connection.host" v-bind="connection"/>
+					</template>
+				</a-table>
+				<br>
+				<a-button type="primary" :disabled="selections.length == 0" :loading="reloading" @click="doReload">Reload selected devices</a-button>
+			</template>
+		</main>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import { AntdComponent } from 'ant-design-vue/types/component';
+	import { Column } from 'ant-design-vue/types/table/column';
+
+	import { Connection, getDeviceConnections } from '@/client/connections';
+	import { DevicesConfigReloadSpec } from '@/server/device';
+	import { compareStrings } from '../device-functions';
+
+	interface Preview extends DevicesConfigReloadSpec {
+		enabled: boolean;
+		git: boolean;
+	}
+
+	type AntTableColumn = Omit<Column, keyof AntdComponent>;
+
+	interface AddRow {
+		key: string;
+		type: 'add';
+		name: string;
+	}
+
+	interface UpdateRemoveRow {
+		key: string;
+		type: 'update' | 'remove';
+		id: string;
+		name: string;
+		lock: string | undefined;
+		build: string | undefined;
+		connections: Connection[];
+	}
+
+	type Row = AddRow | UpdateRemoveRow;
+
+	import { PromiseResult, rootDataComputeds, unwrapPromise } from '../root-data';
+	import SbNavbar from '../components/navbar.vue';
+	import SbConnection from '../components/connection.vue';
+	export default Vue.extend({
+		components: { SbNavbar, SbConnection },
+		computed: {
+			...rootDataComputeds(),
+			columns(): AntTableColumn[] {
+				return [{
+					title: 'Type',
+					dataIndex: 'type',
+					scopedSlots: {
+						customRender: 'type',
+					},
+				}, {
+					title: 'Device',
+					dataIndex: 'name',
+					sorter: (a: Row, b: Row) => compareStrings(a.name, b.name),
+					defaultSortOrder: 'ascend',
+				}, {
+					title: 'Lock',
+					dataIndex: 'lock',
+				}, {
+					title: 'Build',
+					dataIndex: 'build',
+				}, {
+					title: 'Connections',
+					dataIndex: 'connections',
+					scopedSlots: {
+						customRender: 'connections',
+					},
+				}];
+			},
+			rows(): Row[] {
+				const devices = this.devices;
+				let idx = 0;
+				return (this.preview.state === 'resolved' && devices.state === 'resolved') ? [
+					...this.preview.value.add.map<Row>(name => ({
+						key: `${idx++}`,
+						type: 'add',
+						name,
+					})),
+					...Object.entries(this.preview.value.change).map<Row>(([ id, type ]) => {
+						const device = devices.value.find(device => device.id === id)!;
+						return {
+							key: `${idx++}`,
+							type,
+							id,
+							name: device.name,
+							lock: device.jenkinsLockOwner,
+							build: device.build?.name,
+							connections: [...getDeviceConnections(device)],
+						};
+					}),
+				] : [];
+			},
+			defaultSelections(): string[] | undefined {
+				const params = new URLSearchParams(window.location.search);
+				const devices = params.get('devices');
+				return devices ? devices.split(',') : undefined;
+			},
+		},
+		data() {
+			return {
+				preview: { state: 'pending' } as PromiseResult<Preview>,
+				pulled: false as boolean | 'pending',
+				reloading: false,
+				selections: [] as string[],
+			};
+		},
+		mounted() {
+			this.loadConfig();
+		},
+		methods: {
+			loadConfig() {
+				const promise = this.app.service('api/config').get('reload', {});
+				this.preview = unwrapPromise(promise);
+				promise.then(async (spec: DevicesConfigReloadSpec) => {
+					await this.$nextTick();
+					if(this.defaultSelections) {
+						this.selections = this.rows.filter(row => row.type !== 'add' && row.id && this.defaultSelections!.indexOf(row.id) >= 0).map(row => row.key);
+					} else {
+						this.selections = this.rows.map(row => row.key);
+					}
+				});
+			},
+			async gitPull() {
+				this.pulled = 'pending';
+				try {
+					await this.app.service('api/config').patch('reload/pull', {});
+					this.pulled = true;
+					this.loadConfig();
+				} catch(e) {
+					this.pulled = false;
+					this.$notification.close('git-pull');
+					this.$notification.error({
+						key: 'git-pull',
+						duration: 5,
+						placement: 'bottomRight',
+						message: "Git Pull",
+						description: `Failed to git pull: ${e}`,
+					});
+				}
+			},
+			async doReload() {
+				this.reloading = true;
+				const selected = this.rows.filter(row => this.selections.indexOf(row.key) >= 0);
+				const spec: DevicesConfigReloadSpec = {
+					add: selected.filter(row => row.type === 'add').map(row => row.name),
+					change: Object.fromEntries(selected.filter((row): row is UpdateRemoveRow => row.type !== 'add').map(row => [ row.id, row.type ])),
+				};
+				try {
+					await this.app.service('api/config').patch('reload', spec);
+					// Leave 'reloading' set so the button can't be clicked again
+					this.$notification.close('config-reload');
+					this.$notification.success({
+						key: 'config-reload',
+						duration: 0,
+						placement: 'bottomRight',
+						message: "Reload Config",
+						description: "Configuration reloaded",
+					});
+					await new Promise(resolve => setTimeout(resolve, 1000));
+					window.location.href = '/';
+				} catch(e) {
+					this.reloading = false;
+					this.$notification.close('config-reload');
+					this.$notification.error({
+						key: 'config-reload',
+						duration: 10,
+						placement: 'bottomRight',
+						message: "Reload Config",
+						description: `Failed to reload configuration: ${e}`,
+					});
+				}
+			},
+		},
+	});
+</script>
+
+<style lang="less" scoped>
+	.ant-alert {
+		margin-bottom: 10px;
+	}
+</style>

src/client/views/device.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<sb-navbar>
+		<sb-navbar :global-manage="false">
 			<template #brand>
 				<span :title="(device.state == 'resolved') ? device.value.description : undefined">
 					{{ deviceName }}
@@ -26,6 +26,7 @@
 					<a-menu-item v-if="!device.value.jenkinsLockOwner" @click="acquireLock">Reserve in Jenkins</a-menu-item>
 					<a-menu-item v-else @click="releaseLock">Unreserve in Jenkins</a-menu-item>
 				</template>
+				<a-menu-item><a :href="`/config/reload?devices=${id}`">Reload configuration</a></a-menu-item>
 			</a-sub-menu>
 			<a-menu-item class="faux">
 				<sb-lock v-if="showLock && device.state === 'resolved'" ref="lock" :device="device.value" :owner="device.value.jenkinsLockOwner" @close="locking = false"/>
@@ -43,16 +44,7 @@
 				<a-tooltip v-if="focusedNode" placement="bottomRight" :title="`Sending keystrokes to ${focusedNode}`">
 					<i class="fal fa-edit"></i>
 				</a-tooltip>
-				<a-tooltip v-for="connection in connections" :key="connection.host" placement="bottomRight">
-					<template slot="title">
-						<div class="connection name">{{ connection.name }}</div>
-						<div class="connection host" v-if="connection.host != connection.name">{{ connection.host }}</div>
-						<div class="connection nodes">
-							<a-tag v-for="node in connection.nodes" :key="node">{{ node }}</a-tag>
-						</div>
-					</template>
-					<a-avatar shape="square" icon="user" :src="connection.avatar"/>
-				</a-tooltip>
+				<sb-connection v-for="connection in connections" :key="connection.host" v-bind="connection"/>
 			</template>
 		</sb-navbar>
 		<main ref="main">
@@ -125,9 +117,10 @@
 	import SbLock, { SbLockVue } from '../components/lock.vue';
 	import SbLayout, {SbLayoutVue } from '../components/golden-layout.vue';
 	import SbTerminal, { SbTerminalVue } from '../components/terminal.vue';
+	import SbConnection from '../components/connection.vue';
 	import SbCommandModal from '../components/command-modal.vue';
 	export default Vue.extend({
-		components: { SbNavbar, SbCommandMenu, SbJenkins, SbLock, SbLayout, SbTerminal },
+		components: { SbNavbar, SbCommandMenu, SbJenkins, SbLock, SbLayout, SbTerminal, SbConnection },
 		props: {
 			id: {
 				type: String,
@@ -923,21 +916,6 @@
 					margin-left: 5px;
 				}
 			}
-		}
-	}
-
-	.ant-tooltip {
-		.connection.name {
-			font-weight: bold;
-		}
-
-		.connection.host {
-			margin-top: -4px;
-			font-size: smaller;
-		}
-
-		.ant-tag:not(:first-child) {
-			margin-left: 5px;
 		}
 	}
 </style>

src/client/views/home.vue
@@ -1,14 +1,6 @@
 <template>
 	<div class="home">
-		<sb-navbar>
-			<a-sub-menu>
-				<template v-slot:title>
-					<a href="/ports">Ports</a>
-				</template>
-				<a-menu-item><a href="/ports">Full port list</a></a-menu-item>
-				<a-menu-item><a href="/ports/find">Find ports</a></a-menu-item>
-			</a-sub-menu>
-		</sb-navbar>
+		<sb-navbar/>
 		<main>
 			<a-alert v-if="version.state === 'resolved' && version.value.notice" type="info" :message="version.value.notice" show-icon/>
 			<a-tabs :defaultActiveKey="defaultTab" @change="tabSwitched">
@@ -49,16 +41,7 @@
 							</template>
 							<template #connections="connections">
 								<div class="connections">
-									<a-tooltip v-for="connection in connections" :key="connection.host" placement="bottomRight">
-										<template slot="title">
-											<div class="connection name">{{ connection.name }}</div>
-											<div class="connection host" v-if="connection.host != connection.name">{{ connection.host }}</div>
-											<div class="connection nodes">
-												<a-tag v-for="node in connection.nodes" :key="node">{{ node }}</a-tag>
-											</div>
-										</template>
-										<a-avatar shape="square" :size="32" icon="user" :src="connection.avatar" />
-									</a-tooltip>
+									<sb-connection v-for="connection in connections" :key="connection.host" v-bind="connection"/>
 								</div>
 							</template>
 							<template #description="description">
@@ -376,10 +359,11 @@
 	import SbJenkins from '../components/jenkins.vue';
 	import SbFormModal from '../components/form-modal.vue';
 	import SbChangelog from '../components/changelog.vue';
+	import SbConnection from '../components/connection.vue';
 	import { rootDataComputeds, unwrapPromise, PromiseResult } from '../root-data';
 	import { Node, compareStrings, getDeviceUrl, isErrorDevice, uniqifyAndSort, NodeLinkHandler } from '../device-functions';
 	export default Vue.extend({
-		components: { SbNavbar, SbLock, SbJenkins, SbFormModal, SbChangelog },
+		components: { SbNavbar, SbLock, SbJenkins, SbFormModal, SbChangelog, SbConnection },
 		computed: {
 			...rootDataComputeds(),
 			defaultTab(): string {
@@ -917,9 +901,6 @@
 		.connections {
 			position: absolute;
 			top: 12px;
-			> * {
-				margin-right: 5px;
-			}
 		}
 
 		.description {
@@ -1002,21 +983,6 @@
 
 		/deep/ .ant-radio + * {
 			font-weight: bold;
-		}
-	}
-
-	.ant-tooltip {
-		.connection.name {
-			font-weight: bold;
-		}
-
-		.connection.host {
-			margin-top: -4px;
-			font-size: smaller;
-		}
-
-		.ant-tag:not(:first-child) {
-			margin-left: 5px;
 		}
 	}
 

commit c36a9940587088f530080a45aa123f658609da6c

Fix a problem with sending device info to remotes introduced by 813bdd48e27f1d15d5b354db42eb9cb20e1b08a3. Maybe I should write some unit tests...

src/server/web.ts
@@ -48,7 +48,7 @@ function makeServices(app: Application<Services>, config: Config, devices: Devic
 			async find(params) {
 				// If requested, only return local devices. Mainly for requests from remotes.
 				if(params?.query?.local !== undefined) {
-					return devices;
+					return [...devices.map(device => device.toJSON())];
 				}
 				const remoteDeviceLists = await Promise.all(
 					remotes.map(remote => {